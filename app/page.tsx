ad7e873b21931b71f362678bbbf9a85b902cc83e
Zkael03 <azkar986@gmail.com>
2025-04-15 23:44:57 +0700

    Commit Pertama

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+<div className="w-screen h-screen bg-[url('/images/base-bg.jpg')] bg-cover bg-center bg-no-repeat">
+
+
+    </div>
+  );
+}
