20e571d3b8fa34ecd11c9d8402c65611e35468dc
Zkael03 <azkar986@gmail.com>
2025-04-16 10:15:33 +0700

    waktu transisi

--- app/home/components/unduh.tsx
+++ app/home/components/unduh.tsx
@@ -25,21 +25,21 @@ const DownloadButton: React.FC<ButtonProps> = ({
 
   return (
     <a
-      className={`relative group block transition-opacity duration-1000 ease-out ${
+      className={`relative group block transition-opacity duration-[2000ms] ease-out ${
         isVisible ? 'opacity-100' : 'opacity-0'
       }`}
       style={{ width, height }}
-    >
-      <img
-        src={imgSrc}
-        alt="Unduh"
-        className="absolute inset-0 w-full h-full object-contain group-hover:opacity-0 transition-opacity duration-300"
-      />
-      <img
-        src={imgHoverSrc}
-        alt="Unduh Hover"
-        className="absolute inset-0 w-full h-full object-contain opacity-0 group-hover:opacity-100 transition-opacity duration-300"
-      />
+        >
+          <img
+            src={imgSrc}
+            alt="Unduh"
+            className="absolute inset-0 w-full h-full object-contain group-hover:opacity-0 transition-opacity duration-700"
+          />
+          <img
+            src={imgHoverSrc}
+            alt="Unduh Hover"
+            className="absolute inset-0 w-full h-full object-contain opacity-0 group-hover:opacity-100 transition-opacity duration-700"
+        />
     </a>
   );
 };
