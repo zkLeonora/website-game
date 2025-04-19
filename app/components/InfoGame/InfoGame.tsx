9a3fb7606a8309e048b4beecb209c475cf4c4a55
Zkael03 <azkar986@gmail.com>
2025-04-20 03:51:05 +0700

    install font

--- app/components/InfoGame/InfoGame.tsx
+++ app/components/InfoGame/InfoGame.tsx
@@ -1,6 +1,10 @@
-// app/components/infoGame/InfoGameSection.tsx
 'use client';
-import FeatureCard from "./FeatureCard";
+import { Press_Start_2P } from 'next/font/google'
+
+const pressStart = Press_Start_2P({ 
+  subsets: ['latin'], 
+  weight: '400' // Menambahkan properti weight
+});
 
 export default function InfoGameSection() {
   return (
@@ -8,21 +12,26 @@ export default function InfoGameSection() {
       id="info-game"
       className="min-h-screen text-white px-4 py-12"
       style={{
-        backgroundImage: 'url("/images/UI-5.png")', // Ganti dengan path gambar yang sesuai
+        backgroundImage: 'url("/images/UI-5.png")',
         backgroundSize: 'cover',
         backgroundPosition: 'center',
       }}
     >
       <div className="max-w-6xl mx-auto">
-        <h2 className="text-4xl font-bold mb-8 text-center">Apa itu Leyndell?</h2>
-        <p className="text-gray-300 text-center">
-            deskripsi game di sini
+        <h2 className={`text-4xl font-bold mb-8 text-center ${pressStart.className}`}>
+          Apa itu Leyndell?
+        </h2>
+        <p className={`text-gray-300 text-center ${pressStart.className}`}>
+          deskripsi game di sini
         </p>
+
+        {/* 
         <div className="grid md:grid-cols-3 gap-6">
           <FeatureCard title="Eksplorasi Dungeon" description="Masuki dungeon berbahaya dan temukan harta tersembunyi." />
           <FeatureCard title="Beragam Karakter" description="Gunakan berbagai karakter unik dengan skill yang berbeda." />
           <FeatureCard title="Upgrade Senjata" description="Tingkatkan senjatamu untuk menghadapi boss yang kuat!" />
         </div>
+        */}
       </div>
     </section>
   );
