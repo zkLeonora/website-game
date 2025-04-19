438a7d3ec8a85b0c67d63ad1495426516fdb4540
Zkael03 <azkar986@gmail.com>
2025-04-20 01:49:21 +0700

    deskripsi game

--- app/components/InfoGame/InfoGame.tsx
+++ app/components/InfoGame/InfoGame.tsx
@@ -7,10 +7,10 @@ export default function InfoGameSection() {
   return (
     <section id="info-game" className="min-h-screen bg-black text-white px-4 py-12">
       <div className="max-w-6xl mx-auto">
-        <h2 className="text-4xl font-bold mb-8 text-center">Tentang Game</h2>
+        <h2 className="text-4xl font-bold mb-8 text-center">Apa itu Leyndell?</h2>
 
         <p className="text-gray-300 text-center">
-            Setiap petualangan dimulai dari sebuah langkah kecil. Temukan dasar dunia Leyndell lewat panduan RPG di bawah ini.
+            deskripsi game di sini
         </p>
 
         <div className="grid md:grid-cols-3 gap-6">
