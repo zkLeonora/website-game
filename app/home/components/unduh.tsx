d9759a8445d7febfd0e3e2e628a3648b67933591
Zkael03 <azkar986@gmail.com>
2025-07-15 03:44:03 +0700

    update component button download

--- app/home/components/unduh.tsx
+++ app/home/components/unduh.tsx
@@ -1,4 +1,3 @@
-// components/DownloadButton.tsx
 'use client';
 
 import { useEffect, useState } from 'react';
@@ -25,21 +24,23 @@ const DownloadButton: React.FC<ButtonProps> = ({
 
   return (
     <a
+      href="/api/download"
+      download="Leyndell.exe"
       className={`relative group block transition-opacity duration-[2000ms] ease-out ${
         isVisible ? 'opacity-100' : 'opacity-0'
       }`}
       style={{ width, height }}
-        >
-          <img
-            src={imgSrc}
-            alt="Unduh"
-            className="absolute inset-0 w-full h-full object-contain group-hover:opacity-0 transition-opacity duration-700"
-          />
-          <img
-            src={imgHoverSrc}
-            alt="Unduh Hover"
-            className="absolute inset-0 w-full h-full object-contain opacity-0 group-hover:opacity-100 transition-opacity duration-700"
-        />
+    >
+      <img
+        src={imgSrc}
+        alt="Unduh"
+        className="absolute inset-0 w-full h-full object-contain group-hover:opacity-0 transition-opacity duration-700"
+      />
+      <img
+        src={imgHoverSrc}
+        alt="Unduh Hover"
+        className="absolute inset-0 w-full h-full object-contain opacity-0 group-hover:opacity-100 transition-opacity duration-700"
+      />
     </a>
   );
 };
