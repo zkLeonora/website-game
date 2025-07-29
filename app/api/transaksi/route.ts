98eba2213045f01108ef6d032bdf6970540c07a4
Zkael03 <azkar986@gmail.com>
2025-07-29 23:58:28 +0700

    Final Commit - Leyndell

--- app/api/transaksi/route.ts
+++ app/api/transaksi/route.ts
@@ -35,11 +35,12 @@ export async function POST(req: Request) {
 }
 export async function GET() {
   const conn = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'gengkapak12345',
-    database: 'WEBSITE_LEYNDELL',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   });
+
 
   try {
     const [rows] = await conn.query(`
